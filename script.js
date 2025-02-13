39ce4bc3cbd5e468c34d63355423306b0821678b
Joshua Yap <joshuayap2000@gmail.com>
2025-02-13 22:58:52 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let yesButton = document.getElementById("yesButton");
+let noButton = document.getElementById("noButton");
+let menuSection = document.getElementById("menuSection");
+let size = 20;
+
+function yesClicked() {
+    // Hide main container and show menu section
+    document.querySelector(".container").style.display = "none";
+    menuSection.style.display = "block";
+}
+
+function noClicked() {
+    // Move "No" button to a random position
+    let x = Math.random() * (window.innerWidth - 100);
+    let y = Math.random() * (window.innerHeight - 50);
+    noButton.style.left = x + "px";
+    noButton.style.top = y + "px";
+
+    // Increase "Yes" button size
+    size += 10;
+    yesButton.style.fontSize = size + "px";
+}
